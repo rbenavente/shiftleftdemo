053e7ada880f94c2cf6a310487c46b525bd60a95
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-01-26 03:42:36 +0100

    Update waas_attacks.sh

--- files/waas_attacks.sh
+++ files/waas_attacks.sh
@@ -1,5 +1,15 @@
+CHECKSVC=$(kubectl get svc -n evil|grep evil)
+if [[ $CHECKSVC == *"No resources found"* ]]; then
+   echo "nada"
+else
+   kubectl delete svc evilpetclinic -n evil
+fi
+
+kubectl expose deploy evilpetclinic -n evil
+SVC=$(kubectl get svc -n evil|grep evil| awk '{ print $3 }')
+
 echo "testing SQLi attack"
-curl 10.96.220.60?id=%25%27+and+1%3D0+union+select+null%2C+table_name+from+information_schema.tables+%23&Submit=Submit
+curl $SVC?id=%25%27+and+1%3D0+union+select+null%2C+table_name+from+information_schema.tables+%23&Submit=Submit
 if test "$status" == "0"; then
    echo "No error code. Is WAAS configured properly?"
 else
@@ -9,22 +19,22 @@ fi
 sleep 2
 
 echo "testing XSS attack"
-curl 10.96.220.60 -d "<script>alert(1);</script>"
+curl $SVC -d "<script>alert(1);</script>"
 
 echo "testing OS Command Injection attack"
-curl 10.96.220.60 -H "X-Original-Uri: /?id=&%20echo%20aiwefwlguh%20&"
+curl $SVC -H "X-Original-Uri: /?id=&%20echo%20aiwefwlguh%20&"
 
 echo "testing Code Injection attack"
-curl 10.96.220.60 -d "?arg=1; phpinfo()"
+curl $SVC -d "?arg=1; phpinfo()"
 
 echo "testing Local File Inclusion"
-curl 10.96.220.60 -d "8.8.8.8; cat /etc/passwd"
+curl $SVC -d "8.8.8.8; cat /etc/passwd"
 
 echo "testing Shellshock Protection"
-curl 10.96.220.60 --referer "() { :; }; ping -c 3 209.126.230.74"
+curl $SVC --referer "() { :; }; ping -c 3 209.126.230.74"
 
 echo "testing Malformed HTTP Request attack"
-curl -X GET 10.96.220.60 -d "echo 'hello'"
+curl -X GET $SVC -d "echo 'hello'"
 
 echo "Testing DoS protection. (Remember to tweak the DoS settings...)"
-for i in {1..51}; do curl 10.96.220.60; done
+for i in {1..51}; do curl $SVC; done
